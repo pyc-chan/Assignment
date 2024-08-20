3ffb986ea6604b29bdd007db9ceb6d3477d4f000
pyc-chan <rtrtoioil@gmail.com>
2024-08-20 16:23:01 +0900

    Add files via upload

---
+++ Ex09.java
@@ -0,0 +1,25 @@
+package com.ict.day16;
+
+import java.util.HashSet;
+
+public class Ex09 {
+	public static void main(String[] args) {
+		HashSet<Integer> lotto = new HashSet<Integer>();
+		for(int i = 1; i <= 5; i++) {
+			lotto.clear();
+			System.out.print("Lotto Set"+i + ":");
+			while(lotto.size()<7) {
+				int su = (int)(Math.random()*45)+1;
+				if(lotto.add(su)) {
+					if(lotto.size() == 6) {
+						System.out.print(lotto);
+					}else if(lotto.size() == 7){
+						System.out.println(" + Bonus: "+su);
+					}
+				}
+				
+			}
+			
+		}
+	}
+}
